88a1943a4f96b10806259a0cc94bc41cdc1bc371
AceDroo <deroom01@gmail.com>
2023-02-23 20:47:22 +1100

    feat: Adds Combat Move Handler

---
+++ Outback/Outback.Tests/Combat/CombatMoveHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Outback.Combat;
+
+namespace Outback.Tests.Combat
+{
+    public class CombatMoveHandler
+    {
+        private readonly Stack<ICombatMove> _moves;
+
+        public CombatMoveHandler()
+        {
+            _moves = new Stack<ICombatMove>();
+        }
+
+        public void Record(ICombatMove combatMove)
+        {
+            _moves.Push(combatMove);
+            combatMove.Execute();
+        }
+
+        public void Rewind()
+        {
+            if (IsEmpty()) return;
+            var combatMove = _moves.Pop();
+            combatMove.Undo();
+        }
+
+        public bool IsEmpty() => _moves.Count == 0;
+    }
+}
